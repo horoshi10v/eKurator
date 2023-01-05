b8ccaa1117ba1e29f8de03412848d059ab1ddc3f
horoshi10v <valentyn.khoroshylov@nure.ua>
2023-01-05 05:59:01 +0200

    1

---
+++ src/main/java/com/khoroshylov/ekurator/EKuratorApplication.java
@@ -0,0 +1,13 @@
+package com.khoroshylov.ekurator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EKuratorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EKuratorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/khoroshylov/ekurator/EKuratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.khoroshylov.ekurator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EKuratorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
